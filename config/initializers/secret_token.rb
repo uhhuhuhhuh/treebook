112efb5baf71fe10265ebd345398fe102f2519b9
OpenShift guest <ellebrann@gmail.com>
2013-06-13 14:11:05 -0400

    "Created rails application"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '9bd39c0fd6ee46faf12bd5323cc53a7c5021d415d86e8e31cd65ab69e2f8c12bf57f321840427a456c0c2ba38d99215b7e107589dfcb7b3711b74dab18830102'
